8cab65c9326fae2e8eaba6e8c42504cb3c1120c4
Andrés Santamaría <andressantamariac@gmail.com>
2026-06-12 11:10:06 -0500

    actualizar repo

--- C3_listas.py
+++ C3_listas.py
@@ -93,6 +93,94 @@ print(f"longitud deportivos: {variable}")
 # print(motos[8])
 
 #++++++++++++++++++++
-#TRABAJANDO CON LISTA (CAP4)
+#TRABAJANDO CON LISTA (CAPÍTULO 4)
 #++++++++++++++++++++
 
+#Recorrido de listas
+for marcas in deportivos:
+    print(marcas)
+
+#Listas numéricas range(start, (stop-1), step)
+#si se omite step por defecto será 1.
+for value in range(1, 5):
+    print(value, end=" ")
+print("\n")
+
+for value in range(0, 10, 3):
+    print(value)
+
+#crear lista con range
+lista = list(range(1,8))
+print(lista)
+
+lista_impar = list(range(1, 8, 2))
+print(lista_impar)
+
+cuadrados = []
+for numero in range(1,11):
+    cuadrados.append(numero ** 2)
+
+print(cuadrados)
+
+#estadísticas con lista de número
+print(min(cuadrados))
+print(max(cuadrados))
+print(sum(cuadrados))
+
+#lista comprimida, método en una sola línea
+otros_cuadrados = [value**2 for value in range(1,11)]
+print(otros_cuadrados)
+
+
+#partir (slicing) lista[inicio:(fin-1)]
+jugadores = ['elizabeth','gonzalo', 'martina', 'gabriel','norela','andrés']
+print(jugadores)
+print(jugadores[1:5])
+#por defecto inicia en 1
+print(jugadores[:5])
+#también por defecto termina en el último
+print(jugadores[2:])
+#para imprimir los últimos 3 también puedo usar
+print(jugadores[-4:])
+
+#recorriendo bucle a través de un slice
+print("\nEstos son los primeros tres jugadores")
+for jugador in jugadores[:3]:
+    print(jugador.title())
+
+#copiando listas
+copia_jugadores = jugadores[:]
+jugadores.append('Vicente')
+copia_jugadores.append('Carla')
+
+print(jugadores)
+print(copia_jugadores)
+
+#referenciando listas
+copia2_jugadores = jugadores
+jugadores.append('Romina')
+print(jugadores)
+print(copia2_jugadores)
+#   tanto copia2_jugadores como jugadores
+#   son dos nombres con una misma lista 
+#   como referencia.
+
+#
+#  TUPLAS (SON INMUTABLES)
+#
+dimensiones = (200, 100)
+print(dimensiones[0])
+print(dimensiones[1])
+
+#error
+#dimensiones[0] = 280
+#'tuple' object does not support item assigment
+
+#recorrido
+for dimension in dimensiones:
+    print(dimension)
+
+#reasignación de valores a la variable
+
+dimensiones = (280,100)
+print(dimensiones)
